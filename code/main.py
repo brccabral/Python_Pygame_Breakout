6c554f040167658a6a77143a1592520bbf3b3b51
Bruno Cabral <cabral.brc@gmail.com>
2022-03-31 10:23:01 -0700

    player can move

--- code/main.py
+++ code/main.py
@@ -45,6 +45,9 @@ class Game:
                     pygame.quit()
                     sys.exit()
 
+            # update the game
+            self.all_sprites.update(dt)
+
             self.display_surface.blit(self.bg, (0, 0))
             self.all_sprites.draw(self.display_surface)
             # game logic
--- code/sprites.py
+++ code/sprites.py
@@ -15,3 +15,20 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(
             midbottom=(WINDOW_WIDTH // 2, WINDOW_HEIGHT - 20)
         )
+        self.direction = pygame.math.Vector2()
+        self.pos = pygame.math.Vector2(self.rect.topleft)
+        self.speed = 300
+
+    def input(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_RIGHT]:
+            self.direction.x = 1
+        elif keys[pygame.K_LEFT]:
+            self.direction.x = -1
+        else:
+            self.direction.x = 0
+
+    def update(self, dt: float):
+        self.input()
+        self.pos.x += self.direction.x * self.speed * dt
+        self.rect.x = round(self.pos.x)
