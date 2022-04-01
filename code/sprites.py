67aa51813508cb42cfd3c1bfb0c7e7cab90d7835
Bruno Cabral <cabral.brc@gmail.com>
2022-03-31 18:14:42 -0700

    Upgrade class init

--- code/sprites.py
+++ code/sprites.py
@@ -228,3 +228,26 @@ class Block(GameObject):
             )
         else:
             self.kill()
+
+
+class Upgrade(pygame.sprite.Sprite):
+    def __init__(
+        self, groups: List[pygame.sprite.Group], upgrade_type: str, pos: Tuple[int, int]
+    ):
+        super().__init__(groups)
+
+        self.upgrade_type = upgrade_type
+        self.image = pygame.image.load(
+            f"graphics/upgrades/{self.upgrade_type}.png"
+        ).convert_alpha()
+        self.rect = self.image.get_rect(midtop=pos)
+
+        self.pos = pygame.math.Vector2(self.rect.topleft)
+        self.speed = 300
+
+    def update(self, dt: float):
+        self.pos.y += self.speed * dt
+        self.rect.y = round(self.pos.y)
+
+        if self.rect.top > WINDOW_HEIGHT + 100:
+            self.kill()
